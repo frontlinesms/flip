8536dff63d3dcf2df4a4bb6883aa68efecdac216
Alex Anderson <alex@frontlinesms.com>
2012-11-10 17:11:19 +0300

    Added option to have images.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,12 +2,24 @@ import flip.*
 
 class BootStrap {
 	def init = { servletContext ->
+		addStringMetaclassMethods()
+
 		createCyrillicAlphabet()
 		createAmharicAbugida()
+		createMisterMenDeck()
 		createDemoGameAndSesh()
 		createSmallDeckAndGame()
 	}
 	def destroy = {
+	}
+
+	def createMisterMenDeck() {
+		def d = new Deck(name:'Mister Men')
+		d.addToCards(a:'http://img.thesun.co.uk/multimedia/archive/01165/SNF16HHH-280_1165033a.jpg', b:'Mr. Happy')
+		d.addToCards(a:'http://images3.wikia.nocookie.net/__cb20090826034654/mrmen/images/8/8d/MrTickle-1-.gif', b:'Mr. Tickle')
+		d.addToCards(a:'https://si0.twimg.com/profile_images/1012178433/MrTall_bigger.gif', b:'Mr. Tall')
+		d.addToCards(a:'http://www.themistermen.co.uk/images/mrmen_uk/small.gif', b:'Mr. Small')
+		d.save(flush:true, failOnError:true)
 	}
 
 	def createSmallDeckAndGame() {
@@ -122,4 +134,16 @@ class BootStrap {
 		def game = new Game(deck: Deck.findByName('Cyrillic Alphabet')).save(flush: true, failOnError: true)
 		def sesh = new Sesh(game: game, complete: false, cards: game.deck.cards).save(flush: true, failOnError: true)
 	}
+
+	def addStringMetaclassMethods() {
+		String.metaClass.getCardHtml = {
+			if((delegate.startsWith('http://') || delegate.startsWith('https://')) &&
+					(delegate.endsWith('.gif') || delegate.endsWith('.png') || delegate.endsWith('.jpg'))) {
+				return "<img src='$delegate'/>"
+			} else {
+				return delegate
+			}
+		}
+	}
 }
+
