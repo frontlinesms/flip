cd3ec65059e087bc7131c966d63aa4c0eb69b42a
Vaneyck <vaneycksemeotoki@gmail.com>
2012-11-10 14:23:26 +0300

    added rating of Decks

--- grails-app/controllers/flip/DeckController.groovy
+++ grails-app/controllers/flip/DeckController.groovy
@@ -2,4 +2,13 @@ package flip
 
 class DeckController {
 	def scaffold = true
+
+	def rateDeck(){
+		if(params.rating){
+			def deck = Deck.get(params.deckId)
+			deck.rating = ((deck.rating * deck.voteCount) + params.rating) / (deck.voteCount + 1)
+			deck.voteCount += 1
+			deck.save(failOnError:true)
+		}
+	}
 }
--- grails-app/domain/flip/Deck.groovy
+++ grails-app/domain/flip/Deck.groovy
@@ -4,4 +4,6 @@ import org.grails.taggable.*
 class Deck implements Taggable{
 	String name
 	static hasMany = [cards:Card]
+	def rating = 0
+	def voteCount = 0
 }
