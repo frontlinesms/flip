331c0ef8d973fa6867631c61d4f3e4ab4fed10eb
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-11-11 10:14:38 +0300

    users now own decks

--- grails-app/domain/flip/Deck.groovy
+++ grails-app/domain/flip/Deck.groovy
@@ -4,6 +4,11 @@ import org.grails.taggable.*
 class Deck implements Taggable{
 	String name
 	static hasMany = [cards:Card]
+	static belongsTo = [user: User]
+
+	static constraints = {
+		user(nullable: true)
+	}
 
 	String toString() { "DECK[$name]" }
 }
