c70d05dec6ffc7faab192374169dfc201b1dc30f
Alex Anderson <alex@frontlinesms.com>
2012-11-11 15:50:57 +0300

    Added appfog deploy script and configured db more for appfog.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,9 +1,3 @@
-dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
-}
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -11,25 +5,33 @@ hibernate {
 }
 // environment specific settings
 environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
-        }
-    }
+	development {
+		dataSource {
+			pooled = true
+			driverClassName = "org.h2.Driver"
+			username = "sa"
+			password = ""
+			dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+			url = "jdbc:h2:mem:devDb;MVCC=TRUE"
+		}
+	}
+	test {
+		dataSource {
+			pooled = true
+			driverClassName = "org.h2.Driver"
+			username = "sa"
+			password = ""
+			dbCreate = "update"
+			url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+		}
+	}
 	production {
 		def vcapServices = System.env.VCAP_SERVICES
 		def credentials = vcapServices? grails.converters.JSON.parse(vcapServices)["mysql-5.1"][0]["credentials"]: null
 		dataSource {
 			dbCreate = "update"
 			url =  credentials? "jdbc:mysql://${credentials.hostname}:${credentials.port}/${credentials.name}?useUnicode=yes&characterEncoding=UTF-8" :""
-			dialect = org.hibernate.dialect.MySQLInnoDBDialect
+			dialect = org.hibernate.dialect.MySQL5InnoDBDialect
 			driverClassName = "com.mysql.jdbc.Driver"
 			username = credentials? credentials.username: ""
 			password = credentails? credentials.password: ""
